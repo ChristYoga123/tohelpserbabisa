74d7ab8865589ff85641ca41e70bea8b5524cacf
Zainul Muhaimin <zain.muhaimin01@gmail.com>
2025-03-15 16:13:56 +0700

    feat: add OrderHelper class for generating order IDs

---
+++ app/Helpers/OrderHelper.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace App\Helpers;
+
+use App\Models\Transaksi;
+use InvalidArgumentException;
+
+class OrderHelper
+{
+    public static function generateOrderId($prefix)
+    {
+        if (empty($prefix)) {
+            throw new InvalidArgumentException('Prefix is required.');
+        }
+
+        if (!is_string($prefix)) {
+            throw new InvalidArgumentException('Prefix must be a string.');
+        }
+
+        $lastOrder = Transaksi::where('order_id', 'like', $prefix . date('ymd') . '%')
+            ->orderBy('order_id', 'desc')
+            ->first();
+
+        $sequence = 1;
+        if ($lastOrder) {
+            $lastSequence = (int)substr($lastOrder->order_id, -3);
+            $sequence = $lastSequence + 1;
+        }
+
+        return $prefix . date('ymd') . str_pad($sequence, 3, '0', STR_PAD_LEFT);
+    }
+}
